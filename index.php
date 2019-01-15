1937402004fc341d25d7da36107cdf11df5ea281
Kukharenko <bogdan.kukharenko@ekreative.com>
2019-01-15 19:39:00 +0200

    Subtract function

--- index.php
+++ index.php
@@ -8,4 +8,18 @@ function add ($number1, $number2) {
   return "Sum of {$number1} and {$number2}:  {$result}";
 }
 
-echo add(45, 20);
+echo add(45, 20) ?>
+
+<br>
+
+<?php
+
+//Subtract numbers function
+function subtract ($number1, $number2) {
+
+  $result = $number1 - $number2;
+
+  return "Difference between {$number1} and {$number2}:  {$result}";
+}
+
+echo subtract(45, 20); ?>
